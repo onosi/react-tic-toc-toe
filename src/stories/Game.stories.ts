9398a7e93fd00120e3aace14a3470675bc7ec6aa
onosi <onosi.fumiya@gmail.com>
2025-08-11 18:09:58 +0900

    feat: expect以外成功する状態

--- src/stories/Game.stories.ts
+++ src/stories/Game.stories.ts
@@ -1,6 +1,7 @@
 import { Game } from "../App";
-
 import type { StoryObj } from "@storybook/react-vite";
+import { expect, within, userEvent } from "storybook/test";
+import "@testing-library/jest-dom";
 
 const meta = {
   title: "component/Game",
@@ -11,3 +12,18 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {};
+
+export const WinScenario: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const buttons = await canvas.getAllByRole("button");
+
+    await userEvent.click(buttons[0]); // X
+    await userEvent.click(buttons[1]); // O
+    await userEvent.click(buttons[4]); // X
+    await userEvent.click(buttons[2]); // O
+    await userEvent.click(buttons[8]); // X wins
+
+    await expect(canvas.getByText(/Winner: X/i)).toBeInTheDocument();
+  },
+};
